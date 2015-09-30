302aa9e75e2b61ae423be409202e0ed2aa48d5e6
Rafael Cortês <mrfyda@gmail.com>
2015-09-30 19:09:24 +0100

    Release 1.1.0

--- tests.py
+++ tests.py
@@ -2,18 +2,19 @@ import unittest
 import codacy.reporter
 import json
 
+
 class ReporterTests(unittest.TestCase):
 
-  def test_parser(self):
-    def file_get_contents(filename):
-      with open(filename) as f:
-        return f.read()
+    def test_parser(self):
+        def file_get_contents(filename):
+            with open(filename) as f:
+                return f.read()
 
-    jsonContent = file_get_contents('tests/coverage.json')
-    expected = json.loads(jsonContent)
+        jsonContent = file_get_contents('tests/coverage.json')
+        expected = json.loads(jsonContent)
 
-    generated = codacy.reporter.parse_report_file('tests/cobertura.xml')
-    self.assertEqual(generated, expected)
+        generated = codacy.reporter.parse_report_file('tests/cobertura.xml')
+        self.assertEqual(generated, expected)
 
 if __name__ == '__main__':
     unittest.main()
