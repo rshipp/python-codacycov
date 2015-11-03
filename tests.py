7a1be374d4a635ae9b8fc2c726235088f508e9db
Rafael Cortês <mrfyda@gmail.com>
2015-11-03 11:44:35 +0000

    Add support fo coverage 4 and relative git file paths

--- tests.py
+++ tests.py
@@ -4,17 +4,33 @@ import json
 
 
 class ReporterTests(unittest.TestCase):
-
-    def test_parser(self):
+    def compare_parse_result(self, generated_filename, expected_filename):
         def file_get_contents(filename):
             with open(filename) as f:
                 return f.read()
 
-        jsonContent = file_get_contents('tests/coverage.json')
-        expected = json.loads(jsonContent)
+        generated = codacy.reporter.parse_report_file(generated_filename)
 
-        generated = codacy.reporter.parse_report_file('tests/cobertura.xml')
+        json_content = file_get_contents(expected_filename)
+        expected = json.loads(json_content)
+
         self.assertEqual(generated, expected)
+
+    def test_parser_coverage3(self):
+        self.maxDiff = None
+
+        self.compare_parse_result('tests/coverage3/cobertura.xml', 'tests/coverage3/coverage.json')
+
+    def test_parser_coverage4(self):
+        self.maxDiff = None
+
+        self.compare_parse_result('tests/coverage4/cobertura.xml', 'tests/coverage4/coverage.json')
+
+    def test_parser_git_filepath(self):
+        self.maxDiff = None
+
+        self.compare_parse_result('tests/filepath/cobertura.xml', 'tests/filepath/coverage.json')
+
 
 if __name__ == '__main__':
     unittest.main()
